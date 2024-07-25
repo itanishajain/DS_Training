9a452d2aa5fd06f16ce86dceb61ffc3ff663931d
Tanisha Jain <itanishajain@gmail.com>
2024-07-25 20:29:34 +0530

    Web development

--- Web_Development/app.py
+++ Web_Development/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/') # http://127.0.0.1:5000
+@app.route('/') # httpip://127.0.0.1:5000
 def home():
     return render_template('home.html')
 
@@ -14,6 +14,10 @@ def about():
 @app.route('/service') #http://127.0.0.1:5000/service
 def service():
     return render_template('service.html')
+
+@app.route('/cotact') #http://127.0.0.1:5000/contact
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
